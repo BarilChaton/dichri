dc44a9524cfb9bfb890491e3e27670e03d17771f
Karl Christian Karlsson Korbacz <kodman91@gmail.com>
2022-06-11 13:44:40 +0200

    Navbar finished and added icon to tab

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,25 @@
 import React from 'react'
 import {Link} from 'react-scroll'
 import Logo from '../assets/Logo/Logo.png'
-import {GiFlowerTwirl} from "react-icons/gi";
+import {GiFlowerTwirl, GiHamburgerMenu} from "react-icons/gi";
+import { MdMenu, MdMenuOpen } from "react-icons/md";
 import { AiOutlineCheckCircle } from "react-icons/ai";
 import { useState } from 'react'
 
 const Navbar = () => {
+    const [nav, setNav] = useState(false)
+    const handleClick = () => setNav(!nav)
+
   return (
     <div>
         <div className='fixed w-full h-[90px] flex justify-between items-center px-4 border-y-4 border-[#5ce1e6] bg-[#2b8894]'>
-            <div className='mt-[87px] -ml-[22px]'>
-                <a href="/"><img src={Logo} alt="Logo image" style={{width: '175px'}} className='hover:scale-110 duration-300' /></a>
+            <div>
+                <a href="/"><img src={Logo} alt="Logo image" style={{width: '175px'}} className='absolute -mt-[45px] -ml-[22px] hover:scale-110 duration-300 z-10' /></a>
             </div>
 
             {/* Menu */}
-            <ul className='hidden md:flex justify-between items-center gap-[30px]'>
-                <li className='text-2xl text-[#5ce1e6] hover:border-y-2 p-1 rounded-lg hover:text-white duration-100'>
+            <ul className='hidden 2xl:flex justify-between items-center gap-[30px]'>
+                <li className='text-2xl text-[#5ce1e6] hover:border-y-2 p-1 hover:text-white duration-100'>
                 <Link to="hero" smooth={true} duration={500}>
                     Hem
                 </Link>
@@ -23,7 +27,7 @@ const Navbar = () => {
                 <div className='text-[#5ce1e6] animate-spin-slow'>
                     <GiFlowerTwirl />
                 </div>
-                <li className='text-2xl text-[#5ce1e6] hover:border-y-2 p-1 rounded-lg hover:text-white duration-100'>
+                <li className='text-2xl text-[#5ce1e6] hover:border-y-2 p-1 hover:text-white duration-100'>
                 <Link to="hero" smooth={true} duration={500}>
                     Nyheter
                 </Link>
@@ -31,7 +35,7 @@ const Navbar = () => {
                 <div className='text-[#5ce1e6] animate-spin-slow'>
                     <GiFlowerTwirl />
                 </div>
-                <li className='text-2xl text-[#5ce1e6] hover:border-y-2 p-1 rounded-lg hover:text-white duration-100'>
+                <li className='text-2xl text-[#5ce1e6] hover:border-y-2 p-1 hover:text-white duration-100'>
                 <Link to="hero" smooth={true} duration={500}>
                     Produkter
                 </Link>
@@ -39,7 +43,7 @@ const Navbar = () => {
                 <div className='text-[#5ce1e6] animate-spin-slow'>
                     <GiFlowerTwirl />
                 </div>
-                <li className='text-2xl text-[#5ce1e6] hover:border-y-2 p-1 rounded-lg hover:text-white duration-100'>
+                <li className='text-2xl text-[#5ce1e6] hover:border-y-2 p-1 hover:text-white duration-100'>
                 <Link to="hero" smooth={true} duration={500}>
                     Information
                 </Link>
@@ -47,7 +51,7 @@ const Navbar = () => {
                 <div className='text-[#5ce1e6] animate-spin-slow'>
                     <GiFlowerTwirl />
                 </div>
-                <li className='text-2xl text-[#5ce1e6] hover:border-y-2 p-1 rounded-lg hover:text-white duration-100'>
+                <li className='text-2xl text-[#5ce1e6] hover:border-y-2 p-1 hover:text-white duration-100'>
                 <Link to="hero" smooth={true} duration={500}>
                     Vilka Material?
                 </Link>
@@ -55,23 +59,66 @@ const Navbar = () => {
                 <div className='text-[#5ce1e6] animate-spin-slow'>
                     <GiFlowerTwirl />
                 </div>
-                <li className='text-2xl text-[#5ce1e6] hover:border-y-2 p-1 rounded-lg hover:text-white duration-100'>
+                <li className='text-2xl text-[#5ce1e6] hover:border-y-2 p-1 hover:text-white duration-100'>
                 <Link to="hero" smooth={true} duration={500}>
-                    Frågor och Svar
+                    Frågor & Svar
                 </Link>
                 </li>
             </ul>
 
+            {/* Panel under menu */}
+            <div className='hidden 2xl:flex fixed flex-col-2 right-0 top-[90px] w-[1000px] h-[35px] rounded-bl-full place-items-center justify-center gap-[15%] bg-blue-300 text-white'>
+                <p>Mysighet galore!</p>
+                <ul className='flex gap-6'>
+                    <li className='flex place-items-center'><AiOutlineCheckCircle className='mx-2' />Allt handgjort av mig!</li>
+                    <li className='flex place-items-center'><AiOutlineCheckCircle className='mx-2' />100% återvunnen tråd!</li>
+                    <li className='flex place-items-center'><AiOutlineCheckCircle className='mx-2' />Högsta kvalite!</li>
+                </ul>
+            </div>
+
+            {/* Hamburger */}
+            <div onClick={handleClick} className='2xl:hidden absolute right-3 scale-150 text-white z-10'>
+                {!nav ? <MdMenu /> : <MdMenuOpen />}
+            </div>
+
+            {/* Mobile Menu */}
+            <div className='2xl:hidden'>
+                <ul className={!nav ? 'scale-x-0 ease-out duration-150 fixed top-[90px] left-[25px] w-[450px] h-[85%] rounded-lg bg-blue-300 flex flex-col justify-center items-center' :
+                                      'scale-x-100 ease-in duration-150 fixed top-[90px] left-[25px] w-[450px] h-[85%] rounded-lg bg-blue-300 flex flex-col justify-center items-center'}>
+                    <li className='text-3xl text-white border-2 mb-6 rounded-lg bg-[#2b8894] p-3'>
+                        <Link to="hero" smooth={true} duration={500}>
+                            Hem
+                        </Link>
+                    </li>
+                    <li className='text-3xl text-white border-2 mb-6 rounded-lg bg-[#2b8894] p-3'>
+                        <Link to="hero" smooth={true} duration={500}>
+                            Nyheter
+                        </Link>
+                    </li>
+                    <li className='text-3xl text-white border-2 mb-6 rounded-lg bg-[#2b8894] p-3'>
+                        <Link to="hero" smooth={true} duration={500}>
+                            Produkter
+                        </Link>
+                    </li>
+                    <li className='text-3xl text-white border-2 mb-6 rounded-lg bg-[#2b8894] p-3'>
+                        <Link to="hero" smooth={true} duration={500}>
+                            Information
+                        </Link>
+                    </li>
+                    <li className='text-3xl text-white border-2 mb-6 rounded-lg bg-[#2b8894] p-3'>
+                        <Link to="hero" smooth={true} duration={500}>
+                            Vilka Material?
+                        </Link>
+                    </li>
+                    <li className='text-3xl text-white border-2 mb-6 rounded-lg bg-[#2b8894] p-3'>
+                        <Link to="hero" smooth={true} duration={500}>
+                            Frågor & Svar
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+            
         
-        </div>
-        {/* Panel under menu */}
-        <div className='fixed flex flex-col-2 right-0 top-[90px] w-[60%] h-[35px] rounded-bl-full place-items-center justify-center gap-[15%] bg-blue-300 text-white'>
-            <p>Mysighet galore!</p>
-            <ul className='flex gap-6'>
-                <li className='flex place-items-center'><AiOutlineCheckCircle className='mx-2' />Allt handgjort av mig!</li>
-                <li className='flex place-items-center'><AiOutlineCheckCircle className='mx-2' />100% återvunnen tråd!</li>
-                <li className='flex place-items-center'><AiOutlineCheckCircle className='mx-2' />Högsta kvalite!</li>
-            </ul>
         </div>
     </div>
   )
