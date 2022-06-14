82c0a865a01b7109a8038e85d54f6391aaa90fe4
Karl Christian Karlsson Korbacz <kodman91@gmail.com>
2022-06-14 15:56:50 +0200

    corrections on hero and navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Link} from 'react-scroll'
 import Logo from '../assets/Logo/Logo.png'
 import {GiFlowerTwirl, GiHamburgerMenu} from "react-icons/gi";
 import { MdMenu, MdMenuOpen } from "react-icons/md";
 import { AiOutlineCheckCircle } from "react-icons/ai";
-import { useState } from 'react'
+
 
 const Navbar = () => {
     const [nav, setNav] = useState(false)
@@ -18,7 +18,7 @@ const Navbar = () => {
             </div>
 
             {/* Menu */}
-            <ul className='hidden lg:flex justify-between items-center gap-[30px]'>
+            <ul className='hidden xl:flex justify-between items-center gap-[30px]'>
                 <li className='text-lg text-[#5ce1e6] hover:border-y-2 p-1 hover:text-white duration-100'>
                 <Link to="hero" smooth={true} duration={500}>
                     Hem
@@ -67,7 +67,7 @@ const Navbar = () => {
             </ul>
 
             {/* Panel under menu */}
-            <div className='hidden lg:flex fixed flex-col-2 right-0 top-[90px] w-[1000px] h-[35px] rounded-bl-full place-items-center justify-center gap-[15%] bg-blue-300 text-white'>
+            <div className='hidden xl:flex fixed flex-col-2 right-0 top-[90px] w-[1000px] h-[35px] rounded-bl-full place-items-center justify-center gap-[15%] bg-blue-300 text-white'>
                 <p>Mysighet galore!</p>
                 <ul className='flex gap-6'>
                     <li className='flex place-items-center'><AiOutlineCheckCircle className='mx-2' />Allt handgjort av mig!</li>
@@ -83,15 +83,15 @@ const Navbar = () => {
 
             {/* Mobile Menu */}
             <div className='lg:hidden'>
-                <ul className={!nav ? 'scale-x-0 ease-out duration-150 fixed top-[90px] left-[25px] w-[450px] h-[85%] rounded-lg bg-blue-300 flex flex-col justify-center items-center' :
-                                      'scale-x-100 ease-in duration-150 fixed top-[90px] left-[25px] w-[450px] h-[85%] rounded-lg bg-blue-300 flex flex-col justify-center items-center'}>
+                <ul className={!nav ? 'scale-x-0 ease-out duration-150 fixed top-[90px] left-0 w-[450px] h-screen rounded-lg bg-blue-300 flex flex-col place-content-center items-center' :
+                                      'scale-x-100 ease-in duration-150 fixed top-[90px] left-0 w-[450px] h-screen rounded-lg bg-blue-300 flex flex-col place-content-center items-center'}>
                     <li className='text-3xl text-white border-2 mb-6 rounded-lg bg-[#2b8894] p-3'>
-                        <Link to="hero" smooth={true} duration={500}>
+                        <Link onClick={handleClick} to="hero" smooth={true} duration={500}>
                             Hem
                         </Link>
                     </li>
                     <li className='text-3xl text-white border-2 mb-6 rounded-lg bg-[#2b8894] p-3'>
-                        <Link to="news" smooth={true} duration={500}>
+                        <Link onClick={handleClick} to="news" smooth={true} duration={500}>
                             Nyheter
                         </Link>
                     </li>
@@ -101,21 +101,22 @@ const Navbar = () => {
                         </Link>
                     </li>
                     <li className='text-3xl text-white border-2 mb-6 rounded-lg bg-[#2b8894] p-3'>
-                        <Link to="hero" smooth={true} duration={500}>
+                        <Link onClick={handleClick} to="hero" smooth={true} duration={500}>
                             Information
                         </Link>
                     </li>
                     <li className='text-3xl text-white border-2 mb-6 rounded-lg bg-[#2b8894] p-3'>
-                        <Link to="hero" smooth={true} duration={500}>
+                        <Link onClick={handleClick} to="hero" smooth={true} duration={500}>
                             Vilka Material?
                         </Link>
                     </li>
                     <li className='text-3xl text-white border-2 mb-6 rounded-lg bg-[#2b8894] p-3'>
-                        <Link to="hero" smooth={true} duration={500}>
+                        <Link onClick={handleClick} to="hero" smooth={true} duration={500}>
                             Frågor & Svar
                         </Link>
                     </li>
                 </ul>
+                
             </div>
             
         
