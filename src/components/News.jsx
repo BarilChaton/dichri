a652e7154b0c1f751706159484df31de8c7eadf9
Karl Christian Karlsson Korbacz <kodman91@gmail.com>
2022-06-16 13:04:22 +0200

    working on the news section

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -4,7 +4,24 @@ import newsimg1 from '../assets/pictures/news/mak1.jpg'
 const news = () => {
   return (
     <div name='news' className='w-full h-full bg-[#f3c2db] text-white'>
-      <h1>Test header. Please ignore..</h1>
+      {/* news container main */}
+      <div className='flex justify-center items-center'>
+        <h1 className='text-xl'>Nyheter.</h1>
+        <div className='grid grid-rows-3 grid-flow-col gap-4'>
+          {/* object 1 */}
+          <div className='w-90% h-50% bg-slate-500'>
+
+          </div>
+          {/* object 2 */}
+          <div>
+
+          </div>
+          {/* object 3 */}
+          <div>
+
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
